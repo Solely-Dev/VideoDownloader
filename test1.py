de9cb111e0b07228ef2f6fa866e9df2aecae841b
reshma-0014 <72464344+reshma-0014@users.noreply.github.com>
2020-11-11 12:08:48 +0530

    adding for testing purpose

--- test1.py
+++ test1.py
@@ -2,10 +2,10 @@
 from moviepy.editor import *
 import easygui
 
-# loading video 
+#loading video 
 video_path = easygui.fileopenbox()
 clip = VideoFileClip(video_path) 
 
-# saving the clip 
+#saving the clip 
 clip.write_videofile("sample.mp4") 
  
